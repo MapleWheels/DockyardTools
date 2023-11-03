38fac154fcd13ab6dace871e04400956ccb2f912
MapleWheels <njainanan@gmail.com>
2023-11-03 09:13:29 -0400

    Updates.

--- SharedProject/SharedSource/SignalCounter.cs
+++ SharedProject/SharedSource/SignalCounter.cs
@@ -18,33 +18,55 @@ public class SignalCounter : ItemComponent, IClientSerializable, IServerSerializ
     }
     private int _activeOutputs;
 
+    [InGameEditable, Serialize(1f, IsPropertySaveable.Yes, "Cooldown time between switching turrets.")]
+    public float SwitchTime
+    {
+        get => _switchTime;
+        set => _switchTime = value < float.Epsilon ? 0f : value;
+    }
+    private float _switchTime;
+    private float _remainingSwitchTime;
+    
     // internal vars
     private int _counterVal;
-    private bool _updateOutputs;
-    private readonly SimpleSynchroHelper<SignalCounter> _networkHelper;
 
     public SignalCounter(Item item, ContentXElement element) : base(item, element)
     {
-        this._networkHelper = new(this, ReadData, WriteData);
-        this._networkHelper.TicksBetweenUpdates = 4;    // 15 net-tps
+        isActive = true;
     }
 
     public override void ReceiveSignal(Signal signal, Connection connection)
     {
+        if (_remainingSwitchTime >= float.Epsilon)
+            return;
+        
         if (connection.Name.Equals(S_ADVNEXT_IN))
         {
+            if (signal.value == "0")
+                return;
             _counterVal++;
             if (_counterVal >= NumberOfActiveOutputs)
                 _counterVal = 0;
-            _networkHelper.NetworkUpdateReady();
+            _remainingSwitchTime = SwitchTime;
+            SendNetworkUpdateEvent();
+            SendSignalUpdates();
         }
+    }
 
-        _updateOutputs = true;
+    public override void Update(float deltaTime, Camera cam)
+    {
+        if (_remainingSwitchTime < float.Epsilon) 
+            return;
+        _remainingSwitchTime -= deltaTime;
+        if (_remainingSwitchTime < float.Epsilon)
+            _remainingSwitchTime = 0f;
     }
 
     public override void OnMapLoaded()
     {
-        _updateOutputs = true;
+        _counterVal = 0;
+        _remainingSwitchTime = SwitchTime;
+        SendSignalUpdates();
     }
 
     private void SendSignalUpdates()
@@ -53,45 +75,51 @@ public class SignalCounter : ItemComponent, IClientSerializable, IServerSerializ
         {
             item.SendSignal(i == _counterVal ? "1" : "0", $"{S_OUTPUTPREFIX}{i}");
         }
-
-        _updateOutputs = false;
-    }
-
-    public override void Update(float deltaTime, Camera cam)
-    {
-        if (_updateOutputs)
-            SendSignalUpdates();
-        _networkHelper.DelayedNetworkUpdate();
     }
 
     private void ReadData(IReadMessage msg)
     {
+        _counterVal = msg.ReadInt32();
         _activeOutputs = msg.ReadInt32();
     }
 
     private void WriteData(IWriteMessage msg)
     {
+        msg.WriteInt32(_counterVal);
         msg.WriteInt32(_activeOutputs);
     }
 
     public void ClientEventWrite(IWriteMessage msg, NetEntityEvent.IData extraData = null)
     {
-        _networkHelper.WriteData(msg);
+        WriteData(msg);
     }
 
     public void ClientEventRead(IReadMessage msg, float sendingTime)
     {
-        _networkHelper.ReadData(msg);
+        ReadData(msg);
+        SendSignalUpdates();
     }
 
     public void ServerEventRead(IReadMessage msg, Client c)
     {
-        _networkHelper.ReadData(msg);
-        _networkHelper.SendUpdateNextTick();
+        ReadData(msg);
+        SendNetworkUpdateEvent();
+        SendSignalUpdates();
     }
 
     public void ServerEventWrite(IWriteMessage msg, Client c, NetEntityEvent.IData extraData = null)
     {
-        _networkHelper.WriteData(msg);
+        WriteData(msg);
+    }
+    
+    private void SendNetworkUpdateEvent()
+    {
+#if SERVER
+        if (GameMain.Server is not null)
+            item.CreateServerEvent(this);
+#elif CLIENT
+        if (GameMain.Client is not null)
+            item.CreateClientEvent(this);
+#endif
     }
 }
